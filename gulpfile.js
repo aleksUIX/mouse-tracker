3b443e84c6954cc9a63af5ac2b91d5b7de9a0131
aleksUIX <aleksander@uixlimited.com>
2016-01-29 16:36:00 +0000

    dev server set up

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var babel = require('babelify');
+var connect = require('gulp-connect');
+
 
 function compile(watch) {
   var bundler = watchify(browserify('./src/js/entry.js', { debug: true }).transform(babel, {presets: ["es2015"]}));
@@ -16,7 +18,8 @@ function compile(watch) {
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./dist'));
+      .pipe(gulp.dest('./dist'))
+      .pipe(connect.reload());
   }
 
   if (watch) {
@@ -33,7 +36,14 @@ function watch() {
   return compile(true);
 };
 
+gulp.task('connect', function() {
+  connect.server({
+    port: '3000',
+    livereload: true
+  });
+});
+
 gulp.task('build', function() { return compile(); });
 gulp.task('watch', function() { return watch(); });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'connect']);
