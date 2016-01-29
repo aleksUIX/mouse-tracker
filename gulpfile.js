688dec85f191f92abffdc97cbd1260dd1273760f
aleksUIX <aleksander@uixlimited.com>
2016-01-29 16:32:04 +0000

    gulpfile bundler and watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,39 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
 
+function compile(watch) {
+  var bundler = watchify(browserify('./src/js/entry.js', { debug: true }).transform(babel, {presets: ["es2015"]}));
 
-gulp.task('bundle-js', function() {
-  gulp.src('./src/js/entry.js')
-    .pipe(browserify())
-    .pipe(gulp.dest('./dist/'));
-});
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist'));
+  }
 
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
 
-gulp.task('default', ['bundle-js']);
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
