b60bf57310ae2d22f8a03293abdeca3e94a84614
aleksUIX <aleksander@uixlimited.com>
2016-02-01 12:39:29 +0000

    calculating pixel distance

--- src/js/helpers/CalculateDistance.js
+++ src/js/helpers/CalculateDistance.js
@@ -6,7 +6,19 @@ export default class CalculateDistance {
   }
 
   calculate(data) {
-    
-    return data;
+    // x values
+    const xMax = Math.max(data.start[0], data.end[0]);
+    const xMin = Math.min(data.start[0], data.end[0]);
+
+    // y values
+    const yMax = Math.max(data.start[1], data.end[1]);
+    const yMin = Math.min(data.start[1], data.end[1]);
+
+    const xDistance = xMax - xMin;
+    const yDistance = yMax - yMin;
+
+    const pixelDistance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2))
+
+    return Math.round(pixelDistance);
   }
 }
