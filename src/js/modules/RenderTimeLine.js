b764d4191aacb437c94e881b144ec7741614566c
aleksUIX <aleksander@uixlimited.com>
2016-02-03 13:53:26 +0000

    dropped a line chart in, renders with bugs

--- src/js/modules/RenderTimeLine.js
+++ src/js/modules/RenderTimeLine.js
@@ -12,13 +12,12 @@ export default class RenderTimeLine {
 
   }
 
-  render() {
-    var margin = {top: 20, right: 60, bottom: 30, left: 20},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+  render(data) {
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 500 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
-    var parseDate = d3.time.format("%Y-%m-%d").parse,
-        formatDate = d3.time.format("%Y");
+    var formatDate = d3.time.format("%d-%b-%y");
 
     var x = d3.time.scale()
         .range([0, width]);
@@ -28,115 +27,64 @@ export default class RenderTimeLine {
 
     var xAxis = d3.svg.axis()
         .scale(x)
-        .orient("bottom")
-        .tickSize(-height, 0)
-        .tickPadding(6);
+        .orient("bottom");
 
     var yAxis = d3.svg.axis()
         .scale(y)
-        .orient("right")
-        .tickSize(-width)
-        .tickPadding(6);
-
-    var area = d3.svg.area()
-        .interpolate("step-after")
-        .x(function(d) { return x(d.date); })
-        .y0(y(0))
-        .y1(function(d) { return y(d.value); });
+        .orient("left");
 
     var line = d3.svg.line()
-        .interpolate("step-after")
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.value); });
+        .x(function(d) { return x(d.time); })
+        .y(function(d) { return y(d.distance); });
 
-    var svg = d3.select(this.$el).append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    var svg = d3.select(this.$el)
+
+    svg.selectAll('*').remove();
+
+    svg = svg.append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
       .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var zoom = d3.behavior.zoom()
-        .on("zoom", draw);
+    data = data.map(type);
 
-    var gradient = svg.append("defs").append("linearGradient")
-        .attr("id", "gradient")
-        .attr("x2", "0%")
-        .attr("y2", "100%");
+    x.domain(d3.extent(data, function(d) { return d.time; }));
+    y.domain(d3.extent(data, function(d) { return d.distance; }));
 
-    gradient.append("stop")
-        .attr("offset", "0%")
-        .attr("stop-color", "#fff")
-        .attr("stop-opacity", .5);
-
-    gradient.append("stop")
-        .attr("offset", "100%")
-        .attr("stop-color", "#999")
-        .attr("stop-opacity", 1);
-
-    svg.append("clipPath")
-        .attr("id", "clip")
-      .append("rect")
-        .attr("x", x(0))
-        .attr("y", y(1))
-        .attr("width", x(1) - x(0))
-        .attr("height", y(0) - y(1));
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .attr("transform", "translate(" + width + ",0)");
-
-    svg.append("path")
-        .attr("class", "area")
-        .attr("clip-path", "url(#clip)")
-        .style("fill", "url(#gradient)");
 
     svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")");
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Price ($)");
 
     svg.append("path")
+        .datum(data)
+        .attr({
+          class: 'line',
+          d: line,
+          fill: 'none',
+          'stroke-width': 4,
+          stroke: 'black'
+        })
         .attr("class", "line")
-        .attr("clip-path", "url(#clip)");
+        .attr("d", line)
 
-    svg.append("rect")
-        .attr("class", "pane")
-        .attr("width", width)
-        .attr("height", height)
-        .call(zoom);
 
-    var data = [
-      {
-        time: new Date('1988-01-01'),
-        distance: 5
-      },
-      {
-        time: new Date('1988-01-02'),
-        distance: 15
-      },
-      {
-        time: new Date('1988-01-03'),
-        distance: 25
-      }
-    ];
-
-    data.forEach(function(d) {
-      d.date = parseDate(d.time);
-      d.value = + d.distance;
-    });
-
-    x.domain([new Date(1999, 0, 1), new Date(2003, 0, 0)]);
-    y.domain([0, d3.max(data, function(d) { return d.value; })]);
-    zoom.x(x);
-
-    svg.select("path.area").data([data]);
-    svg.select("path.line").data([data]);
-    draw();
-
-    function draw() {
-      svg.select("g.x.axis").call(xAxis);
-      svg.select("g.y.axis").call(yAxis);
-      svg.select("path.area").attr("d", area);
-      svg.select("path.line").attr("d", line);
+    function type(d) {
+      //d.time = formatDate(d.time);
+      d.distance = d.distance;
+      return d;
     }
   }
 }
