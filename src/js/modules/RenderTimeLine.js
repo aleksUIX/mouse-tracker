367bd9f6b074fbbc4076c8373bc42b99c15307da
aleksUIX <aleksander@uixlimited.com>
2016-02-04 16:11:59 +0000

    created a draw method

--- src/js/modules/RenderTimeLine.js
+++ src/js/modules/RenderTimeLine.js
@@ -14,76 +14,69 @@ export default class RenderTimeLine {
 
   render(data) {
     // variable declarations;
-    var margin,
-      width,
-      height,
+    var margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 50
+      },
+      width = 350 - margin.left - margin.right,
+      height = 150 - margin.top - margin.bottom,
       formatDate,
       x,
       y,
       xAxis,
       yAxis,
       line,
-      svg;
+      svg = d3.select(this.$el);
 
     function draw() {
+      formatDate = d3.time.format("%d-%b-%y");
 
-    }
-
-    function update() {
-
-    }
-
-    function drawData() {
-
-    }
-
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 350 - margin.left - margin.right,
-    height = 150 - margin.top - margin.bottom;
-
-    var formatDate = d3.time.format("%d-%b-%y");
-
-    var x = d3.time.scale()
+      x = d3.time.scale()
         .range([0, width]);
 
-    var y = d3.scale.linear()
+      y = d3.scale.linear()
         .range([height, 0]);
 
-    var xAxis = d3.svg.axis()
+      xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom");
-
-    var yAxis = d3.svg.axis()
+      yAxis = d3.svg.axis()
         .scale(y)
         .orient("left");
 
-    var line = d3.svg.line()
-        .x(function(d) { return x(d.time); })
-        .y(function(d) { return y(d.distance); })
-        //.interpolate('monotone');
+      line = d3.svg.line()
+        .x(function(d) {
+          return x(d.time);
+        })
+        .y(function(d) {
+          return y(d.distance);
+        });
 
-    var svg = d3.select(this.$el)
+      // TO DO: we want to only update the path
+      // not the whole chart area
+      svg.selectAll('*').remove();
 
-    svg.selectAll('*').remove();
+      svg = svg.append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg = svg.append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      x.domain(d3.extent(data, function(d) {
+        return d.time;
+      }));
+      y.domain(d3.extent(data, function(d) {
+        return d.distance;
+      }));
 
-    data = data.map(type);
-
-    x.domain(d3.extent(data, function(d) { return d.time; }));
-    y.domain(d3.extent(data, function(d) { return d.distance; }));
-
-
-    svg.append("g")
+      svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    svg.append("g")
+      svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
         .append("text")
@@ -93,7 +86,7 @@ export default class RenderTimeLine {
         .style("text-anchor", "end")
         .text("Price ($)");
 
-    svg.append("path")
+      svg.append("path")
         .datum(data)
         .attr({
           class: 'line',
@@ -105,11 +98,16 @@ export default class RenderTimeLine {
         .attr("class", "line")
         .attr("d", line)
 
+    }
 
-    function type(d) {
-      //d.time = formatDate(d.time);
-      d.distance = d.distance;
-      return d;
+    draw();
+
+    function update() {
+
+    }
+
+    function drawData() {
+
     }
   }
 }
