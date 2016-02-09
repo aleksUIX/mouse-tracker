bd6a8cd92dc39dd0db7c58b6207c5796e990f9eb
aleksander <aleksander@uixlimited.com>
2016-02-09 13:01:53 +0100

    bar rendering work

--- src/js/modules/renderEngines/Bar.js
+++ src/js/modules/renderEngines/Bar.js
@@ -22,6 +22,7 @@ class Bar {
   render(data, svg) {
     // remove old line
     console.log(this.dataSeries);
+    console.log(data);
 
 
     svg.selectAll('.bar')
@@ -29,14 +30,16 @@ class Bar {
 
     svg.selectAll('rect')
       .data(data)
+      .enter()
       .append('rect')
+      .classed('bar', true)
       .attr({
-        // x: ,
-        // y: ,
-        // width: ,
-        // height:
-        // fill: '#CCCCCC'
-      })
+        x: (d) => { return this.dataSeries.x(d.time); },
+        y: (d) => { return -this.dataSeries.y.range()[1] - this.dataSeries.y(d.distance); },
+        width: 10,
+        height: (d) => { return this.dataSeries.y(d.distance); },
+        fill: '#CCCCCC'
+      });
 
   }
 
