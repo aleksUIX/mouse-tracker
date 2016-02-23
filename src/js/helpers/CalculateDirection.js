71544f96dec02119c5148276646156e52c5a76a9
aleksUIX <aleksander@uixlimited.com>
2016-02-23 13:23:49 +0100

    angle calculation ver 1

--- src/js/helpers/CalculateDirection.js
+++ src/js/helpers/CalculateDirection.js
@@ -4,7 +4,9 @@
 class CalculateDirection {
   calculate(data) {
     console.log(data); // [0] = x, [1] = y
-    
+    var theta = Math.atan2(data.end[1] - data.start[1], data.end[0] - data.start[0]); // range (-PI, PI]
+    theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+    console.log(Math.abs(theta));
   }
 }
 
