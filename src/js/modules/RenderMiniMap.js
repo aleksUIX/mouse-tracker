4bdd662342fe066c323a2291da60c4e376c1c6c8
aleksUIX <aleksander@uixlimited.com>
2016-03-03 21:25:42 +0000

    scaffolding and another rename

--- src/js/modules/RenderMiniMap.js
+++ src/js/modules/RenderMiniMap.js
@@ -1,3 +1,16 @@
 // This widget will print the path of the mouse on a canvas that represents the browser window
 // The canvas needs to be consistent with proportions of the browser window
 // It will be a sort of minimap and an introduction to creating a heatmap
+
+
+
+export default class RenderMinimap {
+  constructor() {
+
+  }
+
+  render() {
+    
+  }
+
+}
