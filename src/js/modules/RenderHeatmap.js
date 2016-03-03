64a965c96ff2748145d610f1c63e2d9a6499bab8
aleksUIX <aleksander@uixlimited.com>
2016-03-03 21:22:01 +0000

    heatmap setup

---
+++ src/js/modules/RenderHeatmap.js
@@ -0,0 +1,4 @@
+// This widget will render a heatmap of mouse cursor location
+// The canvas of the heatmap will proportionally represent the browser window
+// on a smaller scale
+// The focus will be on using colour scales to represent the hot points
--- src/js/modules/RenderMiniMap.js
+++ src/js/modules/RenderMiniMap.js
@@ -1,2 +1,3 @@
 // This widget will print the path of the mouse on a canvas that represents the browser window
 // The canvas needs to be consistent with proportions of the browser window
+// It will be a sort of minimap and an introduction to creating a heatmap
