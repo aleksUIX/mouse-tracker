6feee68454042c4ecb6148ad13d9010c4a43ee95
aleksUIX <aleksander@uixlimited.com>
2016-03-03 22:38:44 +0000

    more ideas for heatmaps

--- src/js/modules/RenderHeatmap.js
+++ src/js/modules/RenderHeatmap.js
@@ -2,3 +2,6 @@
 // The canvas of the heatmap will proportionally represent the browser window
 // on a smaller scale
 // The focus will be on using colour scales to represent the hot points
+
+// Feature: heatmap should allow different types of rendering
+// Take into account: square+area, square+colour, spherical+area, spherical+colour, spherical+colour/area, voronoi, voronoi+color; one-dimensional: top-bottom, left-right 
