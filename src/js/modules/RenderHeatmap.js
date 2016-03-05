1637a12c72a91dfdb29757f91aa9a1316b7530c4
aleksUIX <aleksander@uixlimited.com>
2016-03-05 23:15:41 +0000

    bootstrapped heatmap

--- src/js/modules/RenderHeatmap.js
+++ src/js/modules/RenderHeatmap.js
@@ -4,4 +4,19 @@
 // The focus will be on using colour scales to represent the hot points
 
 // Feature: heatmap should allow different types of rendering
-// Take into account: square+area, square+colour, spherical+area, spherical+colour, spherical+colour/area, voronoi, voronoi+color; one-dimensional: top-bottom, left-right 
+// Take into account: square+area, square+colour, spherical+area, spherical+colour, spherical+colour/area, voronoi, voronoi+color; one-dimensional: top-bottom, left-right
+
+import SphereMap from 'SphereMap';
+import SquareMap from 'SquareMap';
+
+
+class RenderHeatmap {
+  constructor() {
+    this.sphereMap = new SphereMap();
+    this.squareMap = new SquareMap();
+  }
+
+
+
+
+}
