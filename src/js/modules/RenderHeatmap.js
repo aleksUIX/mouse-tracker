da2bb0c3683ce0a3376188125d17c996676c72cb
aleksUIX <aleksander@uixlimited.com>
2016-03-14 21:54:59 +0000

    abstracted sphere renderer to separate class

--- src/js/modules/RenderHeatmap.js
+++ src/js/modules/RenderHeatmap.js
@@ -88,56 +88,6 @@ export default class RenderHeatmap {
       exists = true;
     }
 
-
-    function update(data) {
-      series = svg.selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr({
-          cx: (d) => {
-            return x(d.x);
-          },
-          cy: (d) => {
-            return y(d.y);
-          },
-          r: '10',
-          fill: "url(#sphere-gradient)"
-        });
-    }
-
-
-    function defineGradient(svg) {
-      var gradient = svg.append('defs')
-        .append('radialGradient')
-        .attr({
-          id: 'sphere-gradient',
-          cx: '50%',
-          cy: '50%',
-          r: '75%',
-          fx: '50%',
-          fy: '50%'
-        });
-
-      gradient.append('stop')
-        .attr({
-          offset: '0%'
-        })
-        .style({
-          'stop-color': 'rgb(255, 191, 0)',
-          'stop-opacity': 0.4
-        });
-
-      gradient.append('stop')
-        .attr({
-          offset: '90%'
-        })
-        .style({
-          'stop-color': 'rgb(255, 191, 0)',
-          'stop-opacity': 0
-        });
-    }
-
     return function(data) {
       // check if widget is in the DOM
       if (exists) {
--- src/js/modules/mapEngines/SphereMap.js
+++ src/js/modules/mapEngines/SphereMap.js
@@ -2,5 +2,54 @@
 
 
 export default class SphereMap {
+  constructor() {
+    this.update = function(data) {
+      series = svg.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr({
+          cx: (d) => {
+            return x(d.x);
+          },
+          cy: (d) => {
+            return y(d.y);
+          },
+          r: '10',
+          fill: "url(#sphere-gradient)"
+        });
+    }
 
+
+    this.defineGradient = function(svg) {
+      var gradient = svg.append('defs')
+        .append('radialGradient')
+        .attr({
+          id: 'sphere-gradient',
+          cx: '50%',
+          cy: '50%',
+          r: '75%',
+          fx: '50%',
+          fy: '50%'
+        });
+
+      gradient.append('stop')
+        .attr({
+          offset: '0%'
+        })
+        .style({
+          'stop-color': 'rgb(255, 191, 0)',
+          'stop-opacity': 0.4
+        });
+
+      gradient.append('stop')
+        .attr({
+          offset: '90%'
+        })
+        .style({
+          'stop-color': 'rgb(255, 191, 0)',
+          'stop-opacity': 0
+        });
+    }
+  }
 }
